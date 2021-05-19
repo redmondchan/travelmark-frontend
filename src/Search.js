a3c7c0a7d24573695a538ee4d20b1e636dbdb14e
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-18 20:04:06 -0400

    added fetch to populate countries dropdown

--- src/Search.js
+++ src/Search.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Search = () => {
+
+  useEffect(() => {
+    console.log("loaded")
+    fetch('http://localhost:8083/countries')
+  .then(response => response.json())
+  .then(countries => {
+    for (let i = 0; i < countries.length; i++) {
+      let countriesDropdown = document.getElementById("countries")
+      let countryOption = document.createElement("option")
+      countryOption.value = countries[i].name
+      countryOption.id = countries[i].id
+      countriesDropdown.append(countryOption)
+    }
+  });
+  })
+
  return (
    <div>
     <form id="form">
