86a92078b4532e58ee72fa35124fcf26ff36d7d4
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-26 15:32:07 -0400

    imported results and started handleSubmit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 import Search from './Search'
+import Results from './Results'
 
 function App() {
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log("submit")
+  }
+
   return (
     <div>
       <h1>Travelmark</h1>
-      <Search/>
+      <Search handleSubmit={handleSubmit}/>
+      <Results />
     </div>
   );
 }
