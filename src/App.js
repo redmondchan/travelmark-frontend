33dc2cd391651b615f56fe720bedf4443ad778e8
redmond.chan <redmond.chan@fdmgroup.com>
2021-05-31 10:59:43 -0400

    added conditional to handle form submit when an invalid country is inputted

--- src/App.js
+++ src/App.js
@@ -9,29 +9,60 @@ function App() {
 
   function handleSubmit(e){
     e.preventDefault()
+    //if country picked exist in dropdown then check
+    //if city is picked fetch by cities, if not fetch by countries
+    //if country not on dropdown, error message
     console.log(e)
     let selectedCountry = document.getElementById("input__country").value
-    let selectedCity = document.getElementById("input__city").value
-    if(selectedCity == ""){
-      let selectedCountryId = document.querySelector(`[value="${selectedCountry}"]`).id
-      console.log(selectedCountryId)
-      fetch(`http://localhost:8083/articlesByCountry/${selectedCountryId}`)
-        .then(response => response.json())
-        .then(articles => {
-          console.log(articles)
-          setArticles(articles)
-        })
-    } else {
-      let selectedCityId = document.querySelector(`[value="${selectedCity}"]`).id
-      console.log(selectedCityId)
-      fetch(`http://localhost:8083/articlesByCity/${selectedCityId}`)
-        .then(response => response.json())
-        .then(articles => {
-          console.log(articles)
-          setArticles(articles)
-          console.log({articles})
-        })
+    let selectedCountryExist = document.querySelector(`[value="${selectedCountry}"]`)
+    if(selectedCountryExist){
+      document.querySelector(`[value="${selectedCountry}"]`)
+      let selectedCity = document.getElementById("input__city").value
+      //if no city is selected, fetch articles by country
+      if(selectedCity == ""){
+        let selectedCountryId = document.querySelector(`[value="${selectedCountry}"]`).id
+        console.log(selectedCountryId)
+        fetch(`http://localhost:8083/articlesByCountry/${selectedCountryId}`)
+          .then(response => response.json())
+          .then(articles => {
+            setArticles(articles)
+          })
+      //if city is selected, fetch articles by city
+      } else {
+        let selectedCityId = document.querySelector(`[value="${selectedCity}"]`).id
+        console.log(selectedCityId)
+        fetch(`http://localhost:8083/articlesByCity/${selectedCityId}`)
+          .then(response => response.json())
+          .then(articles => {
+            setArticles(articles)
+          })
+      }
+    }else {
+      console.log(selectedCountryExist)
+      console.log("does not exist")
     }
+    // let selectedCountry = document.getElementById("input__country").value
+    // let selectedCity = document.getElementById("input__city").value
+    // if(selectedCity == ""){
+    //   let selectedCountryId = document.querySelector(`[value="${selectedCountry}"]`).id
+    //   console.log(selectedCountryId)
+    //   fetch(`http://localhost:8083/articlesByCountry/${selectedCountryId}`)
+    //     .then(response => response.json())
+    //     .then(articles => {
+    //       console.log(articles)
+    //       setArticles(articles)
+    //     })
+    // } else {
+    //   let selectedCityId = document.querySelector(`[value="${selectedCity}"]`).id
+    //   console.log(selectedCityId)
+    //   fetch(`http://localhost:8083/articlesByCity/${selectedCityId}`)
+    //     .then(response => response.json())
+    //     .then(articles => {
+    //       console.log(articles)
+    //       setArticles(articles)
+    //       console.log({articles})
+    //     })
+    // }
   }
 
   return (
