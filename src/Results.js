3144cd7034cc9665f02e5381f60851f20d3da26b
Redmond Chan <redmondchan92@gmail.com>
2021-06-13 10:29:49 -0400

    added card header semantic ui component

--- src/Results.js
+++ src/Results.js
@@ -4,13 +4,18 @@ import { Card } from 'semantic-ui-react'
 const Results = (props) => {
 
  return (
-   <Card.Group>
-     {props.articles ?
-          props.articles.map(article => <Card fluid color='red' header={article.title} href={article.link}/>)
-       :
-          <Card fluid color ='yellow' header='no results'/>
-     }
-   </Card.Group>
+   <Card>
+    <Card.Content>
+      <Card.Header>Recent Activity</Card.Header>
+    </Card.Content>
+      <Card.Group>
+         {props.articles ?
+              props.articles.map(article => <Card fluid color='red' header={article.title} href={article.link}/>)
+           :
+              <Card fluid color ='yellow' header='no results'/>
+         }
+      </Card.Group>
+    </Card>
  );
 };
 
