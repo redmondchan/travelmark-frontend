029d223c44004d9d0a7c7aa0f33ac82d147a1e52
redmond chan <redmondchan92@gmail.com>
2021-06-24 12:29:00 -0400

    switched onCountryChange to App.js

--- src/App.js
+++ src/App.js
@@ -69,10 +69,75 @@ function App() {
     // }
   }
 
+  const clearCities = (citiesDropdown) => {
+    //clears all city options so cities from other countries are not there when a user selects a different coutnry
+    // let citiesDropdown = document.getElementById("cities")
+    while (citiesDropdown.firstChild) {
+      citiesDropdown.removeChild(citiesDropdown.firstChild);
+    }
+    //clears selected city when user changes country
+    let cityInput = document.getElementById("input__city")
+    cityInput.value = ""
+  }
+
+  const validateCountry = (e, selectedCountryOption) => {
+    console.log(e)
+    let cityInput = document.getElementById("input__city")
+    //finds selected country option
+    //let selectedCountryOption = document.querySelector(`[value="${e.target.value}"]`)
+
+    if (selectedCountryOption) {
+      document.getElementById("input__country").setCustomValidity('')
+      cityInput.removeAttribute("disabled")
+      return true
+    } else {
+      document.getElementById("input__city").setAttribute("disabled", true)
+      document.getElementById("input__country").setCustomValidity('Please select a valid country')
+      return false
+    }
+  }
+
+  const populateCities1 = (e, selectedCountryOption, citiesDropdown) => {
+    fetch(`http://localhost:8083/findCitiesByCountryId/${selectedCountryOption.id}`)
+      .then(response => response.json())
+      .then(cities => {
+        for (let i = 0; i < cities.length; i++) {
+          let cityOption = document.createElement("option")
+          cityOption.value = cities[i].name
+          cityOption.id = cities[i].id
+          citiesDropdown.append(cityOption)
+        }
+
+        //alerts me if a country has cities with the same name
+        let citiesArr = []
+        for (let j = 0; j < cities.length; j++) {
+          citiesArr.push(cities[j].name)
+        }
+        let uniqueCities = [...new Set(citiesArr)]
+        if (citiesArr.length !== uniqueCities.length) {
+          alert("Duplicate Cities")
+        }
+      })
+  }
+  const handleCountryChange = (e) => {
+    let citiesDropdown = document.getElementById("cities")
+    let selectedCountryOption = document.querySelector(`[value="${e.target.value}"]`)
+    clearCities(citiesDropdown)
+    if (validateCountry(e, selectedCountryOption)){
+      populateCities1(e, selectedCountryOption, citiesDropdown)
+    } else {
+      //do something
+      document.getElementById("input__city").setAttribute("disabled", true)
+      document.getElementById("input__country").setCustomValidity('Please select a valid country')
+    }
+  }
+
+
+
   return (
     <div>
       <h1>Travelmark</h1>
-      <Search handleSubmit={handleSubmit}/>
+      <Search handleCountryChange={handleCountryChange}/>
       <Results articles={articles} country={country} city={city}/>
     </div>
   );
